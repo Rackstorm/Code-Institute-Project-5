09eb3b03be4383b00caafff6dc4042af00d8b444
Rackstorm <>
2023-11-24 00:29:40 +0000

    Adding dashboard interface

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
+from app_pages.page_cherry_mildew_detector import page_cherry_mildew_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
+# Create an instance of the app
+app = MultiPage(app_name="Cherry Leaf Mildew Detector")
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Leaf Visualiser", page_leaf_visualizer_body)
+app.add_page("Mildew Detection", page_cherry_mildew_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run()  # Run the app
